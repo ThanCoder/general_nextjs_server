3f02ae55627ef0eec4d5b27b41444d0e8eb1f6b2
thancoder <thancoder2019@gmail.com>
2025-04-01 07:27:17 +0630

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="text-align:center">
+    <div className="text-blue">
      <h1> hello general server</h1>
      <h4>thancoder</h4>
     </div>
